503347848a84a7fac95524b9023d432a98bf70b0
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-07 19:25:35 +0300

    Выполнил первое задание.

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число...");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 100;
+int y = 1000;
+int fp = number / x;
+int sp = number % y;
+string d = Convert.ToString(sp);
+char[] r = d.ToCharArray();
+Array.Reverse(r);
+string s = new String(r);
+int a = Convert.ToInt32(s);
+if (a == fp)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> Да");
+}
+else
+{ 
+   Console.Write(number);
+   Console.WriteLine(" -> Нет");
+}
