bc6761b97b03b6d4df1babd20d0a77efedc6da6e
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-07 20:17:45 +0300

    Выполнил третье  задание.

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число...");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+void Method(int count)
+{
+    while (count >= x)
+    {
+       int y = 3;
+       double s = Math.Pow( x, y);
+       Console.WriteLine(s);
+       x++; 
+    }
+}
+Method(n);
